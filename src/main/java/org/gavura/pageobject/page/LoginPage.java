b2dea15f482010a647a0e70122e8c69b621dc982
Kostiantyn Gavura <gavura@duck.com>
2022-04-18 23:02:46 +0200

    add LoginPage

---
+++ src/main/java/org/gavura/pageobject/page/LoginPage.java
@@ -0,0 +1,33 @@
+package org.gavura.pageobject.page;
+
+import org.gavura.pageobject.BasePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage extends BasePage {
+    @FindBy(id = "login_field")
+    private WebElement loginField;
+    @FindBy(id = "password")
+    private WebElement passwordField;
+    @FindBy(name = "commit")
+    private WebElement signInButton;
+
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public LoginPage open() {
+        driver.get("https://github.com/login");
+
+        return this;
+    }
+
+    public LoginPage login(String email, String password) {
+        loginField.sendKeys(email);
+        passwordField.sendKeys(password);
+        signInButton.click();
+
+        return new HomePage(driver);
+    }
+}
